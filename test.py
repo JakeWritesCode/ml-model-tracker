5b5576a808632ef00f5030a81c48e62e000b64f0
datascience <jake.saunders@equalexperts.com>
2022-09-22 15:11:08 +0100

    Aut commit py files

--- test.py
+++ test.py
@@ -2,4 +2,5 @@ from training_log import ModelTrainingLog
 
 training_log = ModelTrainingLog()
 training_log.create_log_file()
+training_log.commit()
 training_log.check_for_non_log_changes()
--- training_log.py
+++ training_log.py
@@ -13,15 +13,20 @@ class ModelTrainingLog:
         self.log_file_location = log_file or os.path.join(self.git_root, "training_log_file.json")
         self.repo = Repo(self.git_root)
 
-    def commit(self):
+    def commit(self, add_new_files=False):
         """Commit to the git repo and save the hash."""
+        self.repo.index.add("*.ipynb")
+        self.repo.index.add("*.py")
+        message = input("Please give us a nice git commit message.")
+        self.repo.index.commit(message)
+
 
     def check_for_non_log_changes(self):
         """Check that nothing has changed before saving against a given commit hash."""
         index = self.repo.index
         diff = index.diff(None)
         if len(diff) > 0:
-            # Check if it's just the log file thats changed.
+            # Check if it's just the log file that changed.
             if len(diff) == 1:
                 if diff[0].b_path == os.path.relpath(self.log_file_location, self.git_root):
                     return
