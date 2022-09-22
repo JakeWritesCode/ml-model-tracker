936ce6d604adc66c658c286520f088d4da4cc738
datascience <jake.saunders@equalexperts.com>
2022-09-22 15:33:58 +0100

    Added logging functionality

--- test.py
+++ test.py
@@ -3,4 +3,7 @@ from training_log import ModelTrainingLog
 training_log = ModelTrainingLog()
 training_log.create_log_file()
 training_log.commit()
-training_log.check_for_non_log_changes()
+training_log.train_results = {"a": "result"}
+training_log.training_params = {"a": 1}
+training_log.test_results = {"Result": "Very good"}
+training_log.log()
--- training_log.py
+++ training_log.py
@@ -1,3 +1,4 @@
+import json
 import os
 from git import Repo
 
@@ -5,8 +6,11 @@ from git import Repo
 class ModelTrainingLog:
     """A training and configuration log for ML models."""
 
-    current_hash = None
     data_hashes = {}
+    _current_hash = None
+    training_params = {}
+    train_results = {}
+    test_results = {}
 
     def __init__(self, git_root=None, log_file=None):
         self.git_root = git_root or os.path.dirname(os.path.realpath(__file__))
@@ -19,8 +23,7 @@ class ModelTrainingLog:
         self.repo.index.add("*.py")
         message = input("Please give us a nice git commit message.")
         self.repo.index.commit(message)
-        print("Committed latest cvhange")
-
+        print("Committed latest changes, ready to train.")
 
     def check_for_non_log_changes(self):
         """Check that nothing has changed before saving against a given commit hash."""
@@ -39,6 +42,52 @@ class ModelTrainingLog:
         if not os.path.isfile(self.log_file_location) or recreate:
             open(self.log_file_location, 'w')
 
+    def _get_current_hash(self):
+        self._current_hash = self.repo.head.commit.hexsha
+        return self._current_hash
+
+    @property
+    def current_hash(self):
+        self.check_for_non_log_changes()
+        if self._current_hash is None:
+            self._get_current_hash()
+        return self._current_hash
+
+    def log(self, force=False):
+        if not self.training_params:
+            raise IncompleteDataError(
+                "You have not provided training parameter data. "
+                "If this is intentional please use force=True"
+            )
+
+        if not self.train_results:
+            raise IncompleteDataError(
+                "You have not provided training results data. "
+                "If this is intentional please use force=True"
+            )
+        if not self.test_results:
+            raise IncompleteDataError(
+                "You have not provided test results data. "
+                "If this is intentional please use force=True"
+            )
+        log_line = {"training_parameters": self.training_params,
+                    "training_results": self.train_results,
+                    "test_results": self.test_results}
+        self._write_log_to_file(log_line)
+        print("Wrote the training results to the log file.")
+
+    def _write_log_to_file(self, log_line):
+        existing_file = json.load(self.log_file_location)
+        existing_file[self.current_hash] = log_line
+        with open(self.log_file_location, "w") as f:
+            json.dump(existing_file, f, ensure_ascii=False, indent=4)
+
+
+
+
 
 class ChangesMadeError(Exception):
+    pass
+
+class IncompleteDataError(Exception):
     pass
